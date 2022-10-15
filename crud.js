b11f023da48c598524d56d1ba40b02d1140880a3
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2022-10-15 10:49:28 +0530

    remove button added

--- crud.js
+++ crud.js
@@ -4,10 +4,14 @@ let feedbackText = document.querySelector("#feedback-input");
 let parentDiv = document.querySelector("#list");
 let button = document.querySelector("#btn");
 
+function remove(element){
+    element.parentElement.remove();
+}
 function add(){
     if(inputName.value != "" && feedbackText.value != ""){
         let newDiv = document.createElement("div");
-        newDiv.innerHTML = `<p><b>${inputName.value}</b> says ${feedbackText.value}`;
+        newDiv.innerHTML = `<p><b>${inputName.value}</b> says ${feedbackText.value}
+        <button id="removeBtn" onclick="remove(this)">X</button>`;
         parentDiv.appendChild(newDiv);
         inputName.value = "";
         feedbackText.value="";
